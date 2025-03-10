399a5aa0ebfd7e171b2afd9d883e6ea0a9906ceb
codebykuldeep <kuldeep.pal@celestialsys.com>
2025-03-10 14:38:26 +0530

    server update

--- server/src/config.ts
+++ server/src/config.ts
@@ -12,3 +12,10 @@ export const  MAIL_CONFIG={
     NODE_MAIL:process.env.NODE_MAIL,
     NODE_PASS:process.env.NODE_PASS,
 }
+
+export const DB_CONFIG ={
+    PGHOST:process.env.PGHOST,
+    PGDATABASE:process.env.PGDATABASE,
+    PGUSER:process.env.PGUSER,
+    PGPASSWORD:process.env.PGPASSWORD,
+}
